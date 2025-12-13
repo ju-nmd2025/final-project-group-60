12bf9029d69a5679a5d65e7548dc9aa911cf94b0
Denisa-Georgiana <151851868+Denisa-Georgiana-23@users.noreply.github.com>
2025-12-13 16:41:27 +0100

    Add platform movement and breaking logic

--- game.js
+++ game.js
@@ -1,21 +1,31 @@
 import Platform from "./platform";
-import { Character } from "./character";
+import Character from "./character";
 
 //Globals
 let character;
+let platforms = [];
 
 
 //Setup
 function setup() {
   createCanvas(400, 600);
 
-  character = new Character (50, 50, 50, 50);
-  platform = new Platform(250, 230, 80, 20, "normal");
+  //platforms check
+  platforms.push(new Platform(150, 430, 80, 20, "normal"));
+  platforms.push(new Platform(50, 320, 80, 20, "moving"));
+  platforms.push(new Platform(260, 250, 80, 20, "breaking"));
+
+  character = new Character (platforms[0].x + platforms[0].w/ 2-25, platforms[0].y-50, 50, 50);
+
+  
 }
 
 function draw() {
     background(100, 100, 100);
 
     character.draw();
-    platform.draw();
+    for (let p of platforms) {
+      p.update();
+      p.draw();
+    }
 }
--- platform.js
+++ platform.js
@@ -1,20 +1,43 @@
 export default class Platform {
-    constructor(x, y, w, h, type = "normal") {
-        this.x= x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.type= type;
+  constructor(x, y, w, h, type = "normal") {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.type = type;
+
+    this.direction = 1;
+    this.speed = 1.5;
+    this.broken = false;
+  }
+
+  draw() {
+    noStroke();
+
+    if (this.type === "normal") fill(100, 220, 100);
+    else if (this.type === "moving") fill(100, 150, 230);
+    else fill(220, 120, 120);
+
+    rect(this.x, this.y, this.w, this.h, 4);
+  }
+
+  update() {
+    if (this.type === "moving") {
+      this.x += this.speed * this.direction;
+
+      if (this.x < 0 || this.x + this.w > width) {
+        this.direction *= -1;
+      }
     }
 
-    draw() {
-        noStroke();
-
-        if(this.type === "normal") fill(100, 220, 100);
-        else if (this.type === "moving") fill (100, 150, 230);
-        else fill(220, 120, 120);
-
-        rect(this.x, this.y, this.w, this.h, 4);
+    if (this.type === "breaking" && this.broken) {
+      this.y += 4;
     }
+  }
 
+  onJump() {
+    if (this.type === "breaking") {
+      this.broken = true;
+    }
+  }
 }
